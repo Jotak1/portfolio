98ad609018bc0069a91934fc59ffda19ec890e53
JotaK1 <jp.ausensi@gmail.com>
2021-02-28 21:00:30 -0300

    implementacion titulo animado 2

--- src/components/Presentacion.js
+++ src/components/Presentacion.js
@@ -1,23 +1,29 @@
 import styled from '@emotion/styled';
-import './Presentacion.css'
+import './Presentacion.css';
 import React from 'react';
-import Typist from 'react-typist';
+import { Typing, TypingStep } from "typing-effect-reactjs";
 
 const Titulo = styled.div`
+    display: flex;
     font-size: 3rem;
     text-align: center;
     color: #fff;
     align-items: center;
     font-family: 'Squada One', cursive;
     margin: auto;
+    justify-items: center;
 `;
 
 const Presentacion = () => {
     return ( 
         <Titulo>
-        <Typist  >
-            Hola, Soy Juan Pablo <br/>Desarrollador Front End
-      </Typist></Titulo>
+            <Typing
+                text={[
+                    "Hola, Soy Juan Pablo"
+                ]}
+                />
+      
+      </Titulo>
      );
 }
  
