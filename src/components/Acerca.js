48a15dd994092b8a5db556b342085fdcc490e9cc
Jotak1 <jp.ausensi@gmail.com>
2021-03-06 11:02:27 -0300

    implementacion proyectos

--- src/components/Acerca.js
+++ src/components/Acerca.js
@@ -1,54 +1,44 @@
 import { Grid, Slide } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import { Typing } from "typing-effect-reactjs";
 import "../index.css";
-import TagCloud from 'react3dtagcloud'
-
-
-
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    
+    display: "flex",
+    textAlign: "justify",
+    margin: 10,
   },
   Titulo: {
     fontSize: "1.5rem",
-    // textAlign: "center",
-    // margin: "auto",
     color: "#21CBF3",
   },
   content: {
     flexGrow: 1,
-    backgroundColor: '#1d1d1d',
+    backgroundColor: "#1d1d1d",
     padding: theme.spacing(3),
-    color: '#fff',
+    color: "#fff",
   },
   tagstyle: {
-    color: '#fff',
-    display: 'flex',
-    position: "absolute",
-    marginTop: 150,
+    color: "#fff",
+    display: "flex",
+    // // position: "absolute",
+    margin: 20,
   },
-  
 }));
 
 const Acerca = () => {
   const classes = useStyles();
-  const tagName = ['SASS', 'javascript', 'HTML', 'CSS', 'Angular', 'React', 'GIT', 'JSON', 'API', 'Bootstrap']
-  
   return (
     <div className={classes.root}>
-      <Grid container 
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-        <Grid item xs={12} sm={6}>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={12} sm={12}>
           <div className={classes.Titulo}>
-          <Typing text={["Sobre mi"]} element="h1" cursorThickness={0} /></div>
+            <Typing text={["Sobre mi"]} element="h1" cursorThickness={0} />
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <Slide direction="right" in={true}>
             <p>
               Desarrollador Front-End situado en Chile, apasionado por la
@@ -58,18 +48,18 @@ const Acerca = () => {
           <Slide direction="right" in={true}>
             <p>
               Disponible para asumir nuevos desafíos, proactivo, me encanta
-              darle soluciones creativas a los problemas. Fanatico del deporte, 
+              darle soluciones creativas a los problemas. Fanatico del deporte,
               futbol, actividades al aire libre y el ajedrez. Persona de familia
               y padre de una energica niña.
             </p>
           </Slide>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <div className={classes.tagName}>
-          <Slide direction="right" in={true}>
-        <TagCloud speed={1} tagName={tagName} radius={150} ></TagCloud>
-          </Slide>
-        </div>
+          <div className={classes.tagstyle}>
+            <Slide direction="right" in={true}>
+              <p>prueba</p>
+            </Slide>
+          </div>
         </Grid>
       </Grid>
     </div>
--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { Typing } from 'typing-effect-reactjs';
-import '../index.css';
-import { Grid, Slide } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typing } from "typing-effect-reactjs";
+import "../index.css";
+import { Grid, Slide } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  Titulo: {
+    fontSize: "1.5rem",
+    color: "#21CBF3",
+  },
+  Container: {
+    color: "#fff",
+    // textAlign: 'justify'
+  },
+}));
 
 const Proyectos = () => {
-    return ( 
-        <div className="contenedor">
-            <Grid container direction="row" justify="center" alignItems="center">
-                <Grid item xs={12} sm={6}>
-                <Typing text={["Algunos Proyectos"]} element="h1" cursorThickness={0} />
-                    <Slide direction="right" in={true}>
-                        <p>
-                        Desde proyectos
-                        </p>
-                    </Slide>
-                </Grid>
-            </Grid>
-        </div>
-     );
-}
- 
+  const classes = useStyles();
+
+  return (
+    <div className={classes.Container}>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={12} sm={12}>
+          <div className={classes.Titulo}>
+            <Typing
+              text={["Algunos Proyectos"]}
+              element="h1"
+              cursorThickness={0}
+            />
+          </div>
+          
+        </Grid>
+        <Grid item xs={12} sm={12}>
+        <Slide direction="right" in={true}>
+            <p>Desde proyectos</p>
+          </Slide></Grid>
+      </Grid>
+    </div>
+  );
+};
+
 export default Proyectos;
